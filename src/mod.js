f71dd3e1760a05f40a70bf7c2796f03a6493cf37
JustNU <ivancool108@gmail.com>
2023-01-01 08:50:31 +0300

    bump mod dep version, adjust mbav pricing

--- src/mod.js
+++ src/mod.js
@@ -39,17 +39,20 @@ class Mod
 			core.copyBotItemWeighting("AddGearTan_AVS_MBAV", "5aa2a7e8e5b5b00016327c16");
 			
 			// change price
-			database.templates.prices["AddGearTan_AVS_MBAV"] = 139000;
+			database.templates.prices["AddGearTan_AVS_MBAV"] = 118933;
 			
 			for (const handbookItemIndex in database.templates.handbook.Items) {
 				if (database.templates.handbook.Items[handbookItemIndex].Id === "AddGearTan_AVS_MBAV") {
-					database.templates.handbook.Items[handbookItemIndex].Price = 139000;
+					database.templates.handbook.Items[handbookItemIndex].Price = 90465;
 					break;
 				}
 			}
 			
 			// change stats
 			database.templates.items["AddGearTan_AVS_MBAV"]._props.armorClass = database.templates.items["AddGearTan_AVS_MBAV"]._props.armorClass - 1;
+			
+			// add trade offer
+			core.createTraderOffer("AddGearTan_AVS_MBAV", "5ac3b934156ae10c4430e83c", "5449016a4bdc2d6f028b456f", 118933, 3)
 		}
 		
 		// Modify quests
